8836396d4ce89327ebd80976b22e6065b725c72d
Vasek - Tom C <tom.chauveau@epitech.eu>
2021-05-01 17:26:57 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
 	extends: [
 		'airbnb-typescript/base',
 		'plugin:@typescript-eslint/recommended',
-		"prettier/@typescript-eslint",
 		"plugin:prettier/recommended"
 	],
 	rules: {
